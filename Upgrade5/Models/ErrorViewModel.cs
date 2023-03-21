a22e769c41c6ea507b591a0f3505779e5ffebae4
Amit <amitkumar@evincedev.com>
2023-03-21 12:08:19 +0530

    first

---
+++ Upgrade5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Upgrade5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
